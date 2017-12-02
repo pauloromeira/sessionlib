74e2fbab719a483e337c74ab7be59632abc8f6f3
Paulo Romeira <paulo@pauloromeira.com>
2017-12-02 08:43:38 -0300

    Modify Session to AbstractSession - add event methods

--- sessionlib.py
+++ sessionlib.py
@@ -1,30 +1,13 @@
 import logging
 
-from contextlib import AbstractContextManager, ExitStack
+from contextlib import AbstractContextManager
 
 logger = logging.getLogger(__name__)
 
-__all__ = ['Session', 'contextaware']
+__all__ = ['AbstractSession', 'contextaware']
 
 
-def contextaware(func):
-    def func_wrapper(*args, **kwargs):
-        current_session = Session.current()
-
-        if args and isinstance(args[0], Session):
-            session = args[0]
-            if session is current_session:
-                return func(*args, **kwargs)
-            else:
-                with session:
-                    return func(*args, **kwargs)
-        else:
-            return func(current_session, *args, **kwargs)
-
-    return func_wrapper
-
-
-class Session(AbstractContextManager):
+class AbstractSession(AbstractContextManager):
     _sessions = []
 
     @classmethod
@@ -39,33 +22,64 @@ class Session(AbstractContextManager):
     def _pop(cls):
         cls._sessions.pop()
 
-    def __init__(self, *contextmanagers):
-        self._cms = contextmanagers
-        self._exit_stack = ExitStack()
-        self.started = False
+    @property
+    def started(self):
+        getattr(self, '_started', False)
 
+    @started.setter
+    def started(self, value):
+        self._started = value
 
     def __enter__(self):
-        Session._push(self)
+        self.__class__._push(self)
         if self.started:
+            self.on_session_entered()
             logger.info(f'{self} session entered')
             return
 
-        for cm in self._cms:
-            self._exit_stack.enter_context(cm)
-
         self.started = True
+        self.on_session_started()
         logger.info(f'{self} session started')
 
         return self
 
 
     def __exit__(self, *exc_info):
-        Session._pop()
-        if self in Session._sessions:
+        self.__class__._pop()
+        if self in self.__class__._sessions:
+            self.on_session_left()
             logger.info(f'{self} session left')
             return
 
-        self._exit_stack.close()
+        self.on_session_closed()
         logger.info(f'{self} session closed')
 
+
+    def on_session_started(self):
+        pass
+
+    def on_session_entered(self):
+        pass
+
+    def on_session_left(self):
+        pass
+
+    def on_session_closed(self):
+        pass
+
+
+def contextaware(func, session_class=AbstractSession):
+    def func_wrapper(*args, **kwargs):
+        current_session = session_class.current()
+
+        if args and isinstance(args[0], session_class):
+            session = args[0]
+            if session is current_session:
+                return func(*args, **kwargs)
+            else:
+                with session:
+                    return func(*args, **kwargs)
+        else:
+            return func(current_session, *args, **kwargs)
+
+    return func_wrapper
