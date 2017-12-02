d48eaef9a151b28b09fb774f0b3117c9fc7207ef
Paulo Romeira <paulo@pauloromeira.com>
2017-12-02 09:37:45 -0300

    Tweal utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,11 @@ class Observable(object):
         self.callbacks = []
         self.obj = obj
 
-    def subscribe(self, callback):
-        self.callbacks.append(subscribe)
+    def add(self, callback):
+        self.callbacks.append(callback)
+
+    def del(self, callback):
+        self.callbacks.remove(callback)
 
     def __call__(self, args=None):
         for fn in self.callbacks:
