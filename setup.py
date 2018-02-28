5e8142d147459a93ffdb9f4ef642d4d8ef295576
Paulo Romeira <paulo@pauloromeira.com>
2018-02-28 20:28:30 -0300

    New release

--- setup.py
+++ setup.py
@@ -25,13 +25,13 @@ test_requirements = [
 
 setup(
     name='sessionlib',
-    version='0.1.6',
+    version='0.1.7',
     description="Session Library",
     long_description=readme + '\n\n' + history,
     author="Paulo Romeira",
     author_email='paulo@pauloromeira.com',
     url='https://github.com/pauloromeira/sessionlib',
-    download_url='https://github.com/pauloromeira/sessionlib/tarball/0.1.6',
+    download_url='https://github.com/pauloromeira/sessionlib/tarball/0.1.7',
     packages=find_packages(include=['sessionlib']),
     include_package_data=True,
     install_requires=requirements,
